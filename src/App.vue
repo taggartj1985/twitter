966d3df530f500eb732158c2451b90d84331cdf7
John Taggart <taggartj1985@gmail.com>
2020-04-21 18:15:33 +0100

    refactored it on its own file

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,64 @@
 <template lang="html">
+
+  <div>
+    <h1>Twitter</h1>
+    <p>Tweets Today:{{totalTweets}}</p>
+    <twitter-list-item v-for="(tweet,index) in tweets" :key="index" :tweet="tweet"></twitter-list-item>
+  </div>
+</template>
+
+<script>
+  import TwitterListItem from './components/TwitterListItem.vue';
+  export default {
+    name:'app',
+    data() {
+      return {
+        tweets:[
+          {
+            id: 1,
+            name: 'James',
+            handle: '@jokerjames',
+            img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+            tweet: "If you don't succeed, dust yourself off and try again.",
+            likes: 10,
+          },
+          {
+            id: 2,
+            name: 'Fatima',
+            handle: '@fantasticfatima',
+            img: 'https://semantic-ui.com/images/avatar2/large/molly.png',
+            tweet: 'Better late than never but never late is better.',
+            likes: 12,
+          },
+          {
+            id: 3,
+            name: 'Xin',
+            handle: '@xeroxin',
+            img: 'https://semantic-ui.com/images/avatar2/large/elyse.png',
+            tweet: 'Beauty in the struggle, ugliness in the success.',
+            likes: 18,
+          }
+        ]
+        }
+      },
+      components:{
+        'twitter-list-item' : TwitterListItem
+      },
+      computed:{
+        totalTweets: function(){
+          return this.tweets.reduce((total, tweet) => total + tweet.likes, 0);
+         }
+      }
+}
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
+
+<!-- original working -->
+<!-- <template lang="html">
   <div class="app">
 
       <h1>Twitter</h1>
@@ -57,4 +117,4 @@ computed:{
 </script>
 
 <style lang="css" scoped>
-</style>
+</style> -->
---
+++ src/components/TwitterListItem.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="">
+  <h2>{{tweet.name}}</h2>
+  <h3>{{tweet.handle}}</h3>
+  <img v-bind:src=" tweet.img" />
+  <p>Tweet:{{tweet.tweet}}</p>
+  <p>Likes:{{tweet.likes}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'twitter-list-item',
+  props: ['tweet'],
+}
+</script>
+
+<style lang="css" scoped>
+</style>
