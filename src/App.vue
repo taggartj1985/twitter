7d13675a0df72acfbcf262d6ae84da4cce4ca762
John Taggart <taggartj1985@gmail.com>
2020-04-21 16:36:06 +0100

    mvp done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div class="app">
+
+      <h1>Twitter</h1>
+      <section>
+        <div class="tweet" v-for="tweet in tweets">
+          <h2>{{tweet.name}}</h2>
+          <h3>{{tweet.handle}}</h3>
+          <img v-bind:src=" tweet.img" />
+          <p>Tweet:{{tweet.tweet}}</p>
+          <p>Likes:{{tweet.likes}}</p>
+        </div>
+      </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'app',
+  data() {
+  return {
+    tweets:[
+      {
+        id: 1,
+        name: 'James',
+        handle: '@jokerjames',
+        img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+        tweet: "If you don't succeed, dust yourself off and try again.",
+        likes: 10,
+      },
+      {
+        id: 2,
+        name: 'Fatima',
+        handle: '@fantasticfatima',
+        img: 'https://semantic-ui.com/images/avatar2/large/molly.png',
+        tweet: 'Better late than never but never late is better.',
+        likes: 12,
+      },
+      {
+        id: 3,
+        name: 'Xin',
+        handle: '@xeroxin',
+        img: 'https://semantic-ui.com/images/avatar2/large/elyse.png',
+        tweet: 'Beauty in the struggle, ugliness in the success.',
+        likes: 18,
+      }
+    ]
+  };
+},
+// computed:{
+//   totalTweets: function(){
+//     return this.tweets.map((tweet) =>{return tweet.likes});
+//    }
+// }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
---
+++ src/components/TwitterInfo.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+
+</template>
+
+<script>
+export default {
+  name: 'tweet-information'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
